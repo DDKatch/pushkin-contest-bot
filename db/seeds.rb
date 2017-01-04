e1b92e82c32fdbd1d4600332590e987408c95ef5
DDKatch <daniil.field@gmail.com>
2017-01-04 23:32:03 +0300

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+include PoemsHelper
+
+
+URL = "http://rupoem.ru/pushkin/all.aspx"
+
+def parse_poems_from_url(url)
+  doc = Nokogiri::HTML(open(url), nil, "windows-1251")
+  poems = []
+
+  doc.css('h2.poemtitle').each do |link|
+    poems << {title: link.content, text: ""}
+  end
+
+  doc.css('div.poem-text').each_with_index do |link, i|
+    poems[i][:text] = link.content
+  end
+  poems
+end
+
+def init_db(poems)
+  poems = parse_poems_from_url URL
+
+  poems.map do |poem|
+    @poem = Poem.new title: poem[:title]
+    @poem_lines = poem[:text].split "\r\n"# poem lines
+    
+    @poem.save
+    save_poem_lines(Poem.last.id, @poem_lines.reverse)
+  end
+end
+  
+puts "start parse"
+poems = parse_poems_from_url URL
+puts "end parse\nstart initialize database"
+init_db poems
+puts "end initialize database"
