dd12ab410732559188a7839ee95465c0b53935ec
DDKatch <daniil.field@gmail.com>
2017-01-05 12:08:30 +0300

    sql was changed and added cache

--- app/helpers/quiz_helper.rb
+++ app/helpers/quiz_helper.rb
@@ -1,28 +1,21 @@
 module QuizHelper
   def q_resolve(level, question)
+    words = question.split(/[^[[:word:]]]+/)
+    
     case level
     when 1 
       Line.joins(:poem).where(text: question).pluck("title").join   
     when 2
-      line_ids = Word.where(text: question.split(/[^[[:word:]]]+/)).pluck("line_id")
-      freq = line_ids.inject(Hash.new(0)) { |h,v| h[v] += 1; h  }
-      line_id = line_ids.max_by { |v| freq[v]  }
+      line_id = Word.select("line_id").where(text: words).group(:line_id).having("count(*) > 1").order("count(*) desc").limit(1).pluck("line_id").join
       Line.joins(:poem).where(id: line_id).pluck("title").join
     when 3
-      line_ids = Word.where(text: question.split(/[^[[:word:]]]+/)).pluck("line_id")
-      freq = line_ids.inject(Hash.new(0)) { |h,v| h[v] += 1; h  }
-      line_id = line_ids.max_by { |v| freq[v]  }  #TODO need take 2 max
+      line_id = Word.select("line_id").where(text: words).group(:line_id).having("count(*) > 1").order("count(*) desc").limit(1).pluck("line_id").join
       Line.joins(:poem).where(id: line_id).pluck("title").join
     when 4
-      line_ids = Word.where(text: question.split(/[^[[:word:]]]+/)).pluck("line_id")
-      freq = line_ids.inject(Hash.new(0)) { |h,v| h[v] += 1; h  }
-      line_id = line_ids.max_by { |v| freq[v]  }  #TODO need take 3 max
+      line_id = Word.select("line_id").where(text: words).group(:line_id).having("count(*) > 1").order("count(*) desc").limit(1).pluck("line_id").join
       Line.joins(:poem).where(id: line_id).pluck("title").join
     when 5 # sql запрос может быть лучше
-      words = question.split(/[^[[:word:]]]+/)
-      line_ids = Word.where(text: question.split(/[^[[:word:]]]+/)).pluck("line_id")
-      freq = line_ids.inject(Hash.new(0)) { |h,v| h[v] += 1; h  }
-      line_id = line_ids.max_by { |v| freq[v]  } 
+      line_id = Word.select("line_id").where(text: words).group(:line_id).having("count(*) > 1").order("count(*) desc").limit(1).pluck("line_id").join
       all_words = Word.where(line_id: line_id).pluck("text")
       "#{all_words.reject{ |w| words.include? w }.join},#{words.reject{ |w| all_words.include? w  }.join}"
     end
