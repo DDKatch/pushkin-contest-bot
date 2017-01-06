2f73c1ad16b3f161b0f62cf9fffe0bfa8a9c91ee
rbuDeveloper <kirabsuir@gmail.com>
2017-01-06 14:48:40 +0300

    fixed

--- app/helpers/quiz_helper.rb
+++ app/helpers/quiz_helper.rb
@@ -7,45 +7,21 @@ module QuizHelper
     query_part_of_letters = "'%#{letters.join("%' AND text like '%")}%'"
     query_part_of_words = "'#{words.join("%' AND text like '%")}%'"
     case level
-      when 1 
-        answers = {}
-        if $redis.get("1") == nil
-          $redis.set("1", {}.to_json)
-        else
-          answers = eval($redis.get("1"))x
-        end
-        if answers[question] == nil          
-          answers[question] = Line.joins(:poem).where(text: question).pluck("title").join
-          answers.to_json
-          $redis.set("1", answers)
-        end
-        answers[question]
-       # binding.pry
-      when 2
-        line_id = Line.where("text like #{query_part_of_words}")
-        Line.joins(:poem).where(id: line_id).pluck("title").join
-      when 3
-        line_id = Word.select("line_id").where(text: words).group(:line_id).having("count(*) > 2").order("count(*) desc").limit(1).pluck("line_id").join
-        Line.joins(:poem).where(id: line_id).pluck("title").join
-      when 4
-        line_id = Word.select("line_id").where(text: words).group(:line_id).having("count(*) > 2").order("count(*) desc").limit(1).pluck("line_id").join
-        Line.joins(:poem).where(id: line_id).pluck("title").join
-      when 5 # sql запрос может быть лучше
-        line_id = Word.select("line_id").where(text: words).group(:line_id).having("count(*) > 2").order("count(*) desc").limit(1).pluck("line_id").join
-        all_words = Word.where(line_id: line_id).pluck("text")
-        "#{all_words.reject{ |w| words.include? w }.join},#{words.reject{ |w| all_words.include? w  }.join}"
-      when 6
-        line_id = Line.where("text like #{query_part_of_letters}").limit(1)
-        Line.joins(:poem).where(id: line_id).pluck("text").join
-      end  
-    
-    query_part = "'#{words.join("%' AND text like '%")}%'"
-
-    case level
     when 1 
-      Line.joins(:poem).where(text: question).pluck("title").join   
+      answers = {}
+      if $redis.get("1") == nil
+        $redis.set("1", {}.to_json)
+      else
+        answers = eval($redis.get("1"))x
+      end
+      if answers[question] == nil          
+        answers[question] = Line.joins(:poem).where(text: question).pluck("title").join
+        answers.to_json
+        $redis.set("1", answers)
+      end
+      answers[question]
     when 2
-      line_id = Line.where("text like #{query_part}")
+      line_id = Line.where("text like #{query_part_of_words}")
       Line.joins(:poem).where(id: line_id).pluck("title").join
     when 3
       line_id = Word.select("line_id").where(text: words).group(:line_id).having("count(*) > 2").order("count(*) desc").limit(1).pluck("line_id").join
@@ -57,6 +33,9 @@ module QuizHelper
       line_id = Word.select("line_id").where(text: words).group(:line_id).having("count(*) > 2").order("count(*) desc").limit(1).pluck("line_id").join
       all_words = Word.where(line_id: line_id).pluck("text")
       "#{all_words.reject{ |w| words.include? w }.join},#{words.reject{ |w| all_words.include? w  }.join}"
+    when 6
+      line_id = Line.where("text like #{query_part_of_letters}").limit(1)
+      Line.joins(:poem).where(id: line_id).pluck("text").join
+    end  
     end
-  end
 end
