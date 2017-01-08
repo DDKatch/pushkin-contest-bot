0db7357e14d97324321ae23089cd66308fa64651
rbuDeveloper <kirabsuir@gmail.com>
2017-01-08 19:59:58 +0300

    bootstrap+dashboard started

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :poems
-  
+  resources :histories
+  root to: "histories#index"
   post '/quiz', to: 'quiz#resolve'
   post '/registration', to: 'quiz#regisration'
 end
