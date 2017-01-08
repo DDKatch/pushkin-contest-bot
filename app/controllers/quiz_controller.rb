8a6bc11e679823ddef038e21342601a285e4662c
DDKatch <daniil.field@gmail.com>
2017-01-08 06:21:10 +0300

    response changed

--- app/controllers/quiz_controller.rb
+++ app/controllers/quiz_controller.rb
@@ -5,44 +5,21 @@ class QuizController < ApplicationController
   
   def resolve
 
-    @question = params[:question]
-    @task_id = params[:id]
-    @level = params[:level]
+    question = params[:question]
+    level = params[:level]
 
-    @level = 2 if @level.nil? # registration
+    answer = q_resolve level, question
     
-    @answer = q_resolve @level, @question
+    render nothing: true
     
-    if @task_id.nil? 
-      @response = {
-        answer: @answer # registration
-      }
-    else
-      @response = {
-        answer: @answer,
-        token: @token,
-        task_id: @task_id
-      }
-    end
+    uri = URI("http://pushkin-contest.ror.by/quiz")
+    parameters = {
+      answer: answer,
+      token:  ENV['API_KEY'],
+      task_id:  params[:id]
+    }
+    Net::HTTP.post_form(uri, parameters)
 
-    render json: @response
-
-    # render nothing: true
-    #
-    # uri = URI("http://localhost:80")
-    # binding.pry
-    # @response = {
-    #     answer:  @answer,
-    #     token:   'USER_TOKEN',
-    #     task_id: params[:id]
-    # }
-    # Net::HTTP.post_form(uri, @response)
-    #
-  end
-
-  def registration
-    @token = params[:token]
-    resolve
   end
 
 end
