54e78af6a862d2af8ec3f2c5382934f663054568
rbuDeveloper <kirabsuir@gmail.com>
2017-01-08 16:08:56 +0300

    added 7th lvl

--- app/controllers/quiz_controller.rb
+++ app/controllers/quiz_controller.rb
@@ -1,8 +1,6 @@
 class QuizController < ApplicationController
   include QuizHelper
-  
   skip_before_action :verify_authenticity_token
-  
   def resolve
 
     question = params[:question]
@@ -12,13 +10,7 @@ class QuizController < ApplicationController
     
     render nothing: true
     
-    uri = URI("http://pushkin-contest.ror.by/quiz")
-    parameters = {
-      answer: answer,
-      token:  ENV['API_KEY'],
-      task_id:  params[:id]
-    }
-    Net::HTTP.post_form(uri, parameters)
+
 
   end
 
