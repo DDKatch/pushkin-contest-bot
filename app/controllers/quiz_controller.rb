e18d8f265e4111168bf1709d61c03f70e4e29033
DDKatch <daniil.field@gmail.com>
2017-01-09 21:37:56 +0300

    response fixed

--- app/controllers/quiz_controller.rb
+++ app/controllers/quiz_controller.rb
@@ -7,7 +7,7 @@ class QuizController < ApplicationController
     level = params[:level]
     answer = q_resolve level, question
     
-		RespondentWorker.perform_async(answer)  
+		RespondentWorker.perform_async(answer, params[:id])  
 		HistoryWriterWorker.perform_async(answer: answer, question: question, level: level)  
     
 		render json: 'ok' 
--- app/workers/respondent_worker.rb
+++ app/workers/respondent_worker.rb
@@ -1,17 +1,13 @@
 class RespondentWorker
   include Sidekiq::Worker
 
-	ADDR=URI("http://pushkin-contest.ror.by/")	
+  ADDR=URI("http://pushkin.rubyroidlabs.com/quiz")	
   
-  def perform(answer)
+  def perform(answer, task_id)
   
     @http = Net::HTTP.new(ADDR.host)
 
-    # retryable(tries: 3) do
-      data = { answer: answer, token: Rails.application.secrets[:api_key], task_id: params[:id]}
-      @http.post('/quiz', data.to_json, {'Content-Type' =>'application/json'})
-    # end
-
-      binding.pry_remote
+    data = { answer: answer, token: Rails.application.secrets[:api_key], task_id: task_id}
+    @http.post('/quiz', data.to_json, {'Content-Type' =>'application/json'})
   end
 end
