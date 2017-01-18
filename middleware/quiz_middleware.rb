0c148959d98909377c08a0dacb93fc0fd3b19aaf
DDKatch <daniil.field@gmail.com>
2017-01-18 13:06:30 +0300

    level 2 optimized

--- middleware/quiz_middleware.rb
+++ middleware/quiz_middleware.rb
@@ -1,40 +1,59 @@
 class QuizMiddleware
-
-  ADDR=URI("http://pushkin.rubyroidlabs.com/quiz") 
   
+  def normalize_db(string)
+    string.mb_chars.squish!.to_s
+    string.gsub(/[\.\,\!\:\;\?\—]\z/, '')
+  end
+
   def initialize app
+    @token = Rails.application.secrets[:api_key]
+    @address = URI("http://pushkin.rubyroidlabs.com/quiz") 
+
+    @poems = JSON.load(IO.read(Rails.root.join('config', 'the-best-db.json')))
+
+    lines_titles_array = @poems.inject([]) do |lines, data|
+      lines << data['lines'].inject([]) do |res_lines, line|
+        res_lines << [normalize_db(line), data['title']]
+      end
+      lines.flatten!
+    end
+
+    @lines_titles= Hash[*lines_titles_array]
+
+    @lines = @poems.inject([]) do |lines, data|
+      lines << data['lines'].inject([]) do |res_lines, line|
+        res_lines << normalize_db(line)
+      end
+    end.flatten!
+
+    # pry
+    
     @app = app
   end
 
-  def send_answer(parameters, time)
-    Net::HTTP.post_form(ADDR, parameters)
-    puts "Answer sended time: #{Time.now - time}\n\n"
+  def send_answer(answer, task_id)
+    Net::HTTP.post_form(@address, {answer: answer, token: @token, task_id: task_id})
   end
 
   def call env
-    request_catched_time = Time.now 
+    request_catched_time = Time.now
     if env["REQUEST_METHOD"] == "POST" && env["REQUEST_PATH"] == "/quiz" # && env["HTTP_HOST"] == "185.143.172.139"   # for production
     # if env["REQUEST_METHOD"] == "POST" && env["REQUEST_PATH"] == "/quiz"
-      puts "Time: #{request_catched_time}"
+      # puts "Time: #{request_catched_time}"
       puts params = JSON.parse(env["rack.input"].read)
 
-      answer = q_resolve(params['level'], normalize(params['question']))
+      answer = resolve(params['level'], normalize(params['question']))
+      puts "Answer: #{answer}, Id: #{params['id']}"
+      # elapsed_time = Time.now.usec - request_catched_time 
+      # puts "\nAlgorithm time: #{elapsed_time} ms"   # better remove in production
       
-      elapsed_time = Time.now - request_catched_time 
-      puts "\nAlgorithm time: #{elapsed_time} ms"   # better remove in production
+      # temp_time = Time.now
+      # elapsed_time = Time.now - temp_time 
       
-      temp_time = Time.now
+      send_answer(answer, params['id'])
 
-      parameters = {
-        answer: answer,
-        token: Rails.application.secrets[:api_key],
-        task_id:  params['id']
-      }
-      elapsed_time = Time.now - temp_time 
-      Thread.new{ send_answer(parameters, temp_time) }
+      puts "Elapsed time: #{Time.now - request_catched_time} sec\n\n"
       
-      puts parameters
-      puts "Request time: #{elapsed_time} ms"   # better remove in production
       [200, { 'Content-Type' => 'application/json' }, ['ok']]
     else
       @app.call env
@@ -43,16 +62,19 @@ class QuizMiddleware
 
   private
   def normalize(string)
-    # binding.pry
     string.mb_chars.squish!.to_s
   end
 
-  def q_resolve(level, question)
-   
+  def resolve(level, question)
     case level
     when 1
-      # binding.pry 
-      LINES_TILES[question]  
+      @lines_titles[question] 
+    when 2
+      question.gsub!('%WORD%', '([А-Яа-я]+)')
+      @lines.map do |line| 
+        word = line[%r{\A#{question }\z}, 1]
+        return word unless word.nil?
+      end
     end
   end
 end
